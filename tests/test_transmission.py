17701b38b8a5529b06e39915c7671b125734624d
CD Clark III <clifton.clark@gmail.com>
2023-12-14 07:29:49 -0600

    feat: added ocular transmission data from Mainster paper.

--- tests/test_transmission.py
+++ tests/test_transmission.py
@@ -1,7 +1,7 @@
 import pytest
 
-from tissue_properties.optical.ocular_transmission import cie203
-from tissue_properties.optical.ocular_transmission import schulmeister
+from tissue_properties.optical.ocular_transmission import (cie203, mainster,
+                                                           schulmeister)
 from tissue_properties.units import *
 
 
@@ -16,12 +16,18 @@ def test_cie_data():
     assert T_t(Q_(530, "nm")).to("percent").magnitude == pytest.approx(78.67)
     assert T_d(Q_(530, "nm")).to("percent").magnitude == pytest.approx(54.3)
 
-def test_schulmeister_effective_transmission():
 
+def test_schulmeister_effective_transmission():
     T_eff = schulmeister.EffectiveTransmission()
     T_t = cie203.TotalTransmission()
     T_d = cie203.DirectTransmission()
 
     assert T_eff("530 nm", "10000 um") == pytest.approx(0.7867)
-    assert T_eff("530 nm", "0 um") == pytest.approx(0.543,rel=0.055)
-    
+    assert T_eff("530 nm", "0 um") == pytest.approx(0.543, rel=0.055)
+
+
+def test_mainster_transmission():
+    T = mainster.Transmission()
+
+    assert T("530 nm") == pytest.approx(5.421831732026283213e-01)
+    assert T("0.530 um") == pytest.approx(5.421831732026283213e-01)
