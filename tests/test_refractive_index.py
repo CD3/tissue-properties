cbd46d429cc82a348032661fc6b0b873b9f065e5
CD Clark III <clifton.clark@gmail.com>
2025-06-05 16:44:00 -0500

    housekeeping: fix typo in comments.

--- tests/test_refractive_index.py
+++ tests/test_refractive_index.py
@@ -46,8 +46,9 @@ def test_navarro_data():
     assert alpha_4(Q_(656.3, "nm")).magnitude == pytest.approx(0, abs=0.001)
     assert alpha_4(Q_(1014, "nm")).magnitude == pytest.approx(1, abs=0.001)
 
-    # it is possible that there is a type in Table 1 of Navarro. they report 1.367 for the cornea refractive index.
-    # but it is the only one that is off by more than 0.0001 at 589 nm. If we swap the 6 and 7, it matches.
+    # it is possible that there is a typo in Table 1 of Navarro. they report 1.367 for the cornea refractive index.
+    # but it is the only one that is off by more than 0.0001 at 589 nm. If we swap the 6 and 7 (1.367 -> 1.376), it matches.
+    # The other models they compare to also have 1.3771.
     assert n_c(Q_(589, "nm")).magnitude == pytest.approx(1.376, abs=0.0001)
     assert n_a(Q_(589, "nm")).magnitude == pytest.approx(1.3374, abs=0.0001)
     assert n_l(Q_(589, "nm")).magnitude == pytest.approx(1.42, abs=0.0001)
